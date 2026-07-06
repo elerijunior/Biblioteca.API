be6c98e305d1b13cb70266ce8336bb9e258c0b01
Eleri Ferreira <107778775+elerijunior@users.noreply.github.com>
2026-07-06 16:53:30 -0300

    implementaçao do PUT ao Author.DAO

--- DAO/AuthorDAO.cs
+++ DAO/AuthorDAO.cs
@@ -1,3 +1,4 @@
+using System.Reflection.Metadata.Ecma335;
 using Biblioteca.API.Entities;
 using Npgsql;
 
@@ -52,5 +53,29 @@ public class AuthorDAO
 			command.Parameters.AddWithValue("@name", author.Name);
 
 			command.ExecuteNonQuery();
+		}
+
+		public Author? GetById(int id)
+		{
+
+			using var connection = new NpgsqlConnection(connectionString);
+
+			connection.Open();
+
+			string sql = @"SELECT Id, Name FROM Authors WHERE Id = @id";
+
+			using var command = new NpgsqlCommand(sql, connection);
+
+			command.Parameters.AddWithValue("@id", id);
+
+			using var reader = command.ExecuteReader();
+
+			if (reader.Read())
+			{
+			int authorId = reader.GetInt32(0);
+				string name = reader.GetString(1);
+				return Author.Load(authorId, name);
+			}
+			return null;
 		}
 }
